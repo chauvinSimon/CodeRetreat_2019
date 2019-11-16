601ecf00bb516d3358a2606d987f7af09c5c4065
Simon Chauvin <simon.chauvin@esrlabs.com>
2019-11-16 18:57:18 +0100

    feature - add basic Grid + test

--- include/Grid.h
+++ include/Grid.h
@@ -1,6 +1,45 @@
-class Grid{
-    public:
-        bool isEmpty(){
-            return true;
-        };
+#include <map>
+#include <set>
+
+struct Coords {
+  int _x;
+  int _y;
+};
+
+int boolToInt(bool x) { return x ? 1 : 0; }
+
+class Grid {
+public:
+  bool isEmpty() { return _cells.empty(); };
+  void add(Coords cell) { _cells[cell._x].insert(cell._y); };
+
+  bool contains(Coords cell) {
+    auto searchRow = _cells.find(cell._x);
+    if (searchRow != _cells.end()) {
+      const auto &row = _cells[cell._x];
+      auto searchCol = row.find(cell._y);
+      if (searchCol != row.end()) {
+        return true;
+      }
+    }
+    return false;
+  }
+  int nbNeighbours(Coords cell) {
+    int total(0);
+    // same row
+    total += boolToInt(contains({cell._x, cell._y - 1}));
+    total += boolToInt(contains({cell._x, cell._y + 1}));
+    // row under
+    for (int i = -1; i < 2; i++) {
+      total += boolToInt(contains({cell._x - 1, cell._y - i}));
+    }
+    // row above
+    for (int i = -1; i < 2; i++) {
+      total += boolToInt(contains({cell._x + 1, cell._y + i}));
+    }
+    return total;
+  }
+
+private:
+  std::map<int, std::set<int>> _cells;
 };
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,7 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
 #include "Grid.h"
-
+#include "doctest.h"
 
 TEST_CASE("Testing Hello World") {
   std::string message{"Hello World"};
@@ -11,4 +10,31 @@ TEST_CASE("Testing Hello World") {
 TEST_CASE("Testing Default Grid is empty") {
   Grid grid;
   CHECK(grid.isEmpty());
+}
+
+TEST_CASE("Testing Add one alive cell") {
+  Grid grid;
+  grid.add({0, 0});
+  CHECK(!grid.isEmpty());
+}
+
+TEST_CASE("Testing Is cell in grid") {
+  Grid grid;
+  grid.add({1, -1});
+  CHECK(grid.contains({1, -1}));
+  CHECK(!grid.contains({0, -1}));
+}
+
+TEST_CASE("Testing Return number of neighbours") {
+  Grid grid;
+  CHECK(grid.isEmpty());
+  grid.add({-1, -1});
+  grid.add({-1, 0});
+  grid.add({-1, 1});
+  grid.add({1, -1});
+  grid.add({1, 0});
+  grid.add({1, 1});
+  grid.add({0, -1});
+  grid.add({0, 1});
+  CHECK(grid.nbNeighbours({0, 0}) == 8);
 }
